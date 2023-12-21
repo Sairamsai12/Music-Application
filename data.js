b74a762d98220bd07e18add4acb31e917cacd8a1
Sairam Karampuri <133942416+Sairamsai12@users.noreply.github.com>
2023-12-21 20:25:51 +0530

    uploaded all files

---
+++ data.js
@@ -0,0 +1,94 @@
+let songs = [
+    {
+        name: 'Achyutham Keshavam',
+        path: 'assets/musics/Achyutam Keshavam.mp3',
+        artist: 'Madhuraa Bhattacharya',
+        cover: 'assets/images/achyutham-keshavam.jpeg' 
+    },
+    {
+        name: 'Agar Tum Saath Ho',
+        path: 'assets/musics/Agar Tum Saath Ho.mp3',
+        artist: 'Arjith Singh, Alka Yagnik',
+        cover: 'assets/images/agar-thum.jpeg'
+    },
+    {
+        name: 'Baby',
+        path: 'assets/musics/Baby.mp3',
+        artist: 'Justin Bieber',
+        cover: 'assets/images/baby-jb.jpeg'
+    },
+    {
+        name: 'Choosa Choosa',
+        path: 'assets/musics/Choosa Choosa.mp3',
+        artist: 'Padmalatha, Sniggy',
+        cover: 'assets/images/druva.jpeg'
+    },
+    {
+        name: 'Diwali Deepaanni',
+        path: 'assets/musics/Diwali Deepaanni.mp3',
+        artist: 'Andrea, Kalyan',
+        cover: 'assets/images/dhada.jpeg'
+    },
+    {
+        name: 'Telisiney Na Nuvvey',
+        path: 'assets/musics/Telisiney Na Nuvvey.mp3',
+        artist: 'Revanth',
+        cover: 'assets/images/arjun reddy.jpeg'
+    },
+    {
+        name: 'Perfect',
+        path: 'assets/musics/Ed Sheeran Perfect.mp3',
+        artist: 'Ed Sheeran',
+        cover: 'assets/images/perfect.jpeg'
+    },
+    {
+        name: 'Evarevaro',
+        path: 'assets/musics/Evarevaro.mp3',
+        artist: 'Vishal Mishra',
+        cover: 'assets/images/animal.jpeg'
+    },
+    {
+        name: 'Kalalo Kooda',
+        path: 'assets/musics/Kalalo Kooda.mp3',
+        artist: 'Sid Sriram, Sagar, & Vaishnavi Kovvuri',
+        cover: 'assets/images/liger.jpeg'
+    },
+    {
+        name: 'Kollagotte',
+        path: 'assets/musics/Kollagotte.mp3',
+        artist: 'Anirudh Ravichander',
+        cover: 'assets/images/remo.jpeg'
+    },
+    {
+        name: 'Emitemitemito',
+        path: 'assets/musics/Emitemitemito.mp3',
+        artist: 'Alphons Joseph',
+        cover: 'assets/images/arjun reddy.jpeg'
+    },
+    {
+        name: 'Manishi Musugulo',
+        path: 'assets/musics/Manishi Musugulo.mp3',
+        artist: 'Hiphop Tamizha, Kaushik Krish, Yadagiri',
+        cover: 'assets/images/druva.jpeg'
+    },
+    {
+        name: 'Rooba Rooba',
+        path: 'assets/musics/Rooba Rooba.mp3',
+        artist: 'Shahil Hada, Chinmayee',
+        cover: 'assets/images/orange.jpeg'
+    },
+    {
+        name: 'The Nights - Avicii',
+        path: 'assets/musics/The Nights - Avicii.mp3',
+        artist: 'Nicholas Furlong',
+        cover: 'assets/images/the nights.jpeg'
+    },
+    {
+        name: 'Yean Ennai Pirindhaai',
+        path: 'assets/musics/Yean-Ennai-Pirindhaai.mp3',
+        artist: 'Sid Sriram',
+        cover: 'assets/images/adithya varma.jpeg'
+    },
+    
+]
+
